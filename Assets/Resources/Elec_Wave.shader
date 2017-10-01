2243c9926376bf1da64d3c1b0a432c608466a756
Gabriel Cun <cvoidstudio@hotmail.com>
2017-09-30 23:56:06 -0400

    30/09/2017
    Latest Upgraded: Wavelike Particle Rendering Functions

---
+++ Assets/Resources/Elec_Wave.shader
@@ -0,0 +1,48 @@
+﻿Shader "Elec_Wave" {
+    SubShader{
+        Pass{
+        
+
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+		GLSLPROGRAM
+
+        uniform float Win_width;
+        uniform float Win_height;
+        #ifdef VERTEX
+        out vec4 View_Port;
+        out vec4 Ver_Color;
+
+        void main()
+        {
+            gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+            Ver_Color = gl_Color;
+        }
+
+        #endif
+
+
+        #ifdef FRAGMENT
+        in vec4 View_Port;
+        in vec4 Ver_Color;
+
+        uniform vec4 Elec_Frag;
+        void main()
+        {
+            gl_FragColor = Ver_Color;
+
+            // Linear Interpolation
+            float X = (gl_FragCoord.x - 0.5) / Win_width;
+            float Y = (Elec_Frag[3] - Elec_Frag[1])*(X - Elec_Frag[0]) / (Elec_Frag[2] - Elec_Frag[0]);
+            Y += Elec_Frag[1];
+            float Diff = 1.0 - abs(((gl_FragCoord.y - 0.5) / Win_height) - Y);
+            gl_FragColor.a *= smoothstep(0.95 + (1.0 - Diff) / 20.0, 1.0, Diff) * Diff * 0.3;
+            gl_FragColor.a += smoothstep(0.992, 1.0, Diff) * Diff;
+        }
+
+        #endif
+
+        ENDGLSL
+        }
+    }
+}
---
+++ Assets/TestMain.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// using OpenGL ES 2.0
+
+public class TestMain : MonoBehaviour {
+    public Material Mat_Target;
+    private Shader glsl_Script;
+    
+
+    // Mesh -> Material : pointer
+    float start_T;
+
+    private int Precise_map = 0;
+    private float Gap = 0F;
+    bool T;
+    void Start () {
+        Precise_map = Screen.width / 6;
+
+        // Bind Shader
+        //glsl_Script = Shader.Find("Elec_Wave");
+
+        //Mat_Target = new Material(glsl_Script);
+        //Mat_Target = new Material(glsl_Script);
+        //Mat_Target.shader = Resources.Load<Shader>("Elec_Wave");
+        //Mat_Target.SetInt("_ZTest", (int)UnityEngine.Rendering.CompareFunction.Always);
+        //Mat_Target.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+        Mat_Target.SetFloat("Win_width", (float)Screen.width);
+        Mat_Target.SetFloat("Win_height", (float)Screen.height);
+
+        start_T = Time.time;
+
+        Sin_Array = new float[Precise_map+1];
+        Elec_Array = new float[Precise_map + 1];
+
+        Gap = 1F / (float)(Precise_map);
+
+        Debug.Log("HHEUIHFosdfoierngi!!!");
+    }
+
+    
+
+    // Update is called once per frame
+    private float Amp = 0.15F;
+    private float[] Sin_Array;
+    private float[] Elec_Array;
+    void Update () {
+        float Now = Time.time - start_T;
+        float Center = 0.5F;
+
+        for (int i = 0; i < Precise_map; ++i)
+        {
+            float X = i * Gap;
+            Sin_Array[i] = (1F - 1.25f*Mathf.Abs((X - Center)))
+                * (Amp * Mathf.Sin((X - Center) *40.0f + Now * 20.0f))
+                + 0.5f;
+            Elec_Array[i] = (1F - 1.25f * Mathf.Abs((X - Center)))
+                *(Amp/2.0f * Mathf.Sin((X - Center) * 20.0f + Now * 40.0f))
+                + 0.5f;
+            Elec_Array[i] *= Random.Range(0.90f, 1.1f);
+        }
+
+        Sin_Array[Precise_map] = Sin_Array[Precise_map-1];
+        Elec_Array[Precise_map] = Elec_Array[Precise_map - 1];
+
+    }
+
+    void OnGUI()
+    {
+        GL.Clear(true, true, Color.black);
+        float X = 0F;
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        X = 0F;
+        for (int i = 0; i < Precise_map/2; ++i)
+        {
+
+            GL.Begin(GL.QUADS);
+            Mat_Target.SetVector("Elec_Frag",
+                new Vector4(X, Elec_Array[i], X + Gap, Elec_Array[i + 1]));
+            Mat_Target.SetPass(0);
+            GL.Color(new Color(0F, 1.0F, 0.3F, 0.5f));
+            GL.Vertex3(X + Gap, 1F, 1F);
+            GL.Color(new Color(0F, 1.0F, 0.3F, 0.5f));
+            GL.Vertex3(X + Gap, 0F, 1F);
+            GL.Color(new Color(0F, 1.0F, 0.3F, 1.0F));
+            GL.Vertex3(X, 0F, 1F);
+            GL.Color(new Color(0F, 1.0F, 0.3F, 1.0F));
+            GL.Vertex3(X, 1F, 1F);
+
+            GL.End();
+            X += Gap;
+        }
+        GL.PopMatrix();
+
+
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        X = 0F;
+        for (int i = 0; i < Precise_map / 2; ++i)
+        {
+            
+            GL.Begin(GL.QUADS);
+            Mat_Target.SetVector("Elec_Frag",
+                new Vector4(X, Sin_Array[i], X + Gap, Sin_Array[i+1]));
+            Mat_Target.SetPass(0);
+            GL.Color(new Color(0.2F, 0.9F, 1.0F, 1F));
+            GL.Vertex3(X + Gap, 1F, 1F);
+            GL.Color(new Color(0.4F, 0.2F, 1.0F, 1F));
+            GL.Vertex3(X + Gap, 0F, 1F);
+            GL.Color(new Color(0.4F, 0.2F, 1.0F, 1F));
+            GL.Vertex3(X, 0F, 1F);
+            GL.Color(new Color(0.2F, 0.9F, 1.0F, 1F));
+            GL.Vertex3(X, 1F, 1F);
+
+            GL.End();
+            X += Gap;
+        }
+        GL.PopMatrix();
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        X = 0F;
+        for (int i = 0; i < Precise_map / 2; ++i)
+        {
+
+            GL.Begin(GL.QUADS);
+            Mat_Target.SetVector("Elec_Frag",
+                new Vector4(X, 1F - Sin_Array[i], X + Gap, 1F - Sin_Array[i + 1]));
+            Mat_Target.SetPass(0);
+            GL.Color(new Color(1.0F, 0.9F, 0.2F, 1F));
+            GL.Vertex3(X + Gap, 1F, 1F);
+            GL.Color(new Color(1.0F, 0.2F, 0.2F, 1F));
+            GL.Vertex3(X + Gap, 0F, 1F);
+            GL.Color(new Color(1.0F, 0.2F, 0.2F, 1F));
+            GL.Vertex3(X, 0F, 1F);
+            GL.Color(new Color(1.0F, 0.9F, 0.2F, 1F));
+            GL.Vertex3(X, 1F, 1F);
+
+            GL.End();
+            X += Gap;
+        }
+        GL.PopMatrix();
+
+
+       
+        
+    }
+}
